03a5a5ecc754b4805a7aaefe5bb5d3d6b9542ba3
Avesh Raj Singh <aveshrajsingh3@gmail.com>
2025-08-27 00:26:20 +0530

    setUP the enviorment variables and connected to mongodb

---
+++ backend/src/config/db.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+import { ENV } from "./env.js";
+
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(ENV.MONGODB_URI);
+        console.log("MongoDB connected successfully!!!");
+    } catch (error) {
+        console.error("MongoDB connection error:", error);
+        throw error;
+    }
+}
+
+export default connectDB;
