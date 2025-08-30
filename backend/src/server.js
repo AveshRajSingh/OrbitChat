83d839ff466e7f5a67d2d0829068f0dc5316dce1
Avesh Raj Singh <aveshrajsingh3@gmail.com>
2025-08-30 12:40:17 +0530

    feat: integrate Inngest for user synchronization and management

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,14 +2,33 @@ import express from "express";
 import { ENV } from "./config/env.js";
 const app = express();
 import connectDB from "./config/db.js";
-
+import {clerkMiddleware} from "@clerk/express"
+import { functions, inngest } from "./config/inngest.js";
+import {serve} from "inngest/express"
 connectDB();
 
 const port = ENV.PORT;
-console.log(port);
+app.use(express.json());
+app.use(clerkMiddleware());
+
+app.use("/api/inngest", serve({ client: inngest, functions }));
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.listen(port, () => console.log(`Server is running on port ${port}`));
+
+const startServer = async () => {
+  try {
+    await connectDB();
+    if(ENV.NODE_ENV !== "production"){
+      app.listen(port, () => console.log(`Server is running on port ${port}`));
+    }
+  } catch (error) {
+    console.error("Error starting server:", error);
+  }
+}
+
+startServer();
+
+export default app;
